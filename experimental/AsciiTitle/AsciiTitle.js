3a13b60f03dffb0fd608c00ae7c0d4e97b267676
kiransuren <kirank.suren@gmail.com>
2021-02-22 18:56:36 -0500

    clean and add customization

--- experimental/AsciiTitle/AsciiTitle.js
+++ experimental/AsciiTitle/AsciiTitle.js
@@ -5,13 +5,31 @@ import MainContext from '../../MainContext'
 //DONT MESS WITH THE TEMPLATE STRING
 const AsciiTitle = () => {
   const api = useContext(MainContext);
+  const [name, setName] = useState("");
   const [toggle,setToggle] = useState(false);
   const [props, set, stop] = useSpring(() => ({opacity: 1, transform: 'translate3d(0px,0,0) scale(0) rotateX(0deg)'}));
           //setProj(!proj);
         //set({width: '100rem', from: {width: '0rem'},
         //config: {mass:10}
         //})
-  var m;
+  const text ="KIRAN+SURENDRAN";
+  const font ="doom";
+  useEffect(() => {
+    
+    const Http = new XMLHttpRequest();
+    const url=`https://cors-anywhere.herokuapp.com/https://artii.herokuapp.com/make?text=${text}&font=${font}`;
+    Http.open("GET", url);
+    Http.setRequestHeader('Access-Control-Allow-Origin', '*');
+    Http.setRequestHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+    Http.setRequestHeader('Access-Control-Allow-Methods', 'Origin, Content-Type, X-Auth-Token');
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+      console.log(Http.responseText)
+      setName(Http.responseText)
+    }
+  }, [])
+
   useEffect(() => {
     if(api.currentPage() != "HOME"){
       console.log("ASCII");
@@ -46,21 +64,16 @@ const AsciiTitle = () => {
   }
 
   useEffect(() => {
-       setTimeout(typeWriter, 1000);
+       //setTimeout(typeWriter, 1000);
     }, []
   );
+  
 
   return(
         <animated.div id="titleparent" style={props} className={api.currentPage()}>
           <p id="tagline"></p>
           <pre id="title">
-{`__  _______ _____            _   _      _____ _    _ _____  ______ _   _ _____  _____            _   _ 
-| |/ /_   _|  __ \\     /\\   | \\ | |    / ____| |  | |  __ \\|  ____| \\ | |  __ \\|  __ \\     /\\   | \\ | |
-| ' /  | | | |__) |   /  \\  |  \\| |   | (___ | |  | | |__) | |__  |  \\| | |  | | |__) |   /  \\  |  \\| |
-|  <   | | |  _  /   / /\\ \\ | . \` |    \\___ \\| |  | |  _  /|  __| | . \` | |  | |  _  /   / /\\ \\ | . \` |
-| . \\ _| |_| | \\ \\  / ____ \\| |\\  |    ____) | |__| | | \\ \\| |____| |\\  | |__| | | \\ \\  / ____ \\| |\\  |
-|_|\\_\\_____|_|  \\_\\/_/    \\_\\_| \\_|   |_____/ \\____/|_|  \\_\\______|_| \\_|_____/|_|  \\_\\/_/    \\_\\_| \\_|
-`}
+          {name}
           </pre>
         </animated.div>
     )
